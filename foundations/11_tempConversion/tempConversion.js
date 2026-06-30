a85ddad1951361613d240974ca6e2f7cd62d32a8
h3-albakar <hamzehbakkar123@gmail.com>
2026-06-30 23:51:37 +0100

    4 of the 6 tests pass in the current implementation

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
 const convertToCelsius = function(degree) {
+  return (5/9) * (degree-32)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  return Math.round((degree * (9/5) + 32) *10)/10
 };
 
 // Do not edit below this line
