e3e0e8bb1ae78469e0d6d247d947e030166daafa
h3-albakar <hamzehbakkar123@gmail.com>
2026-07-21 19:49:58 +0100

    confirmed getComputerChoice works and added event listener to test

--- lessons/rock_paper_scissors/script.js
+++ lessons/rock_paper_scissors/script.js
@@ -1,6 +1,12 @@
-function getComputerChoice(num_choices) {
-    const num = Math.random() * 3
+function getComputerChoice() {
+    const num = Math.random() * 3;
     if (num <= 1) return "Rock";
     else if (num <= 2) return "Paper";
     else if (num <= 3) return "scissors";
 } 
+
+const btn = document.querySelector('#bob');
+
+btn.addEventListener("click", (e) => {
+    console.log(getComputerChoice())
+})
