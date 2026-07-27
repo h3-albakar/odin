07b9eebaf3be0b9f4f94c9919f9e42da162a6035
h3-albakar <hamzehbakkar123@gmail.com>
2026-07-27 19:31:54 +0100

    I added a game feature so that you can play the game for 5 rounds and nested two functions in each other which looked a bit weird but it worked okay :)

--- lessons/rock_paper_scissors/script.js
+++ lessons/rock_paper_scissors/script.js
@@ -1,8 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
-
 const available_choices = ["Rock", "Paper", "Scissors"];
 const human_wins = [["Rock", "Scissors"], ["Paper","Rock"], ["Scissors", "Paper"]];
+
 function getComputerChoice() {
     let index = Math.floor(Math.random() * available_choices.length);
     return available_choices[index];
@@ -12,18 +10,34 @@ function getHumanChoice() {
     let choice = prompt("what are you choosing mate ?!");
     return choice;
 }
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    if (humanChoice === computerChoice) return "its a tie!";
-        for (let item of human_wins){
-            if (humanChoice === item[0] && computerChoice === item[1]) {
-                humanScore += 1;
-                return `human wins`;}}
-    computerScore += 1;
-    return `computer wins`};
-    
+    // Logic for playing one round
+    function playRound(humanChoice, computerChoice) {
+      // If both human choice and computer are the same return early
+      if (humanChoice === computerChoice) return "its a tie!";
+           // otherwise move through every item and check if they are the same as what you have
+           for (let item of human_wins){
+               if (humanChoice === item[0] && computerChoice === item[1]) {
+                   humanScore += 1;
+                    console.log(`human wins round`);}}
+        computerScore += 1;
+    console.log(`computer wins round`)};
+
+    for (let i = 0; i <= 4; i++) {
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (computerScore < humanScore) return 'Human wins bitch';
+    else if (computerScore > humanScore) return 'Computer wins beepbop';
+    else return 'its tie...damn';}
+
 const btn = document.querySelector('#bob');
-
 btn.addEventListener("click", (e) => {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    console.log(playGame());
 })
