a6403791d48a8b9322ec0fbf03c2c9786f1904bf
h3-albakar <hamzehbakkar123@gmail.com>
2026-07-28 09:04:41 +0100

    I made the new change of adding three buttons but I hate that I had to repeat the function three times, we will see if there is a way to make this better eg.querySelectorAll?

--- lessons/rock_paper_scissors/script.js
+++ lessons/rock_paper_scissors/script.js
@@ -1,43 +1,50 @@
 const available_choices = ["Rock", "Paper", "Scissors"];
 const human_wins = [["Rock", "Scissors"], ["Paper","Rock"], ["Scissors", "Paper"]];
 
+let humanScore = 0;
+let computerScore = 0;
+let humanSelection = '';
+let computerSelection = '';
+
 function getComputerChoice() {
     let index = Math.floor(Math.random() * available_choices.length);
     return available_choices[index];
 } 
 
-function getHumanChoice() {
-    let choice = prompt("what are you choosing mate ?!");
-    return choice;
-}
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+  // If both human choice and computer are the same return early
+  if (humanChoice === computerChoice) return "its a tie!";
+       // otherwise move through every item and check if they are the same as what you have
+       for (let item of human_wins){
+           if (humanChoice === item[0] && computerChoice === item[1]) {
+               humanScore += 1;
+                console.log(`human wins round`);
+                return;
+           }
+       }
+    computerScore += 1;
+    console.log(`computer wins round`);
+};
 
-    // Logic for playing one round
-    function playRound(humanChoice, computerChoice) {
-      // If both human choice and computer are the same return early
-      if (humanChoice === computerChoice) return "its a tie!";
-           // otherwise move through every item and check if they are the same as what you have
-           for (let item of human_wins){
-               if (humanChoice === item[0] && computerChoice === item[1]) {
-                   humanScore += 1;
-                    console.log(`human wins round`);}}
-        computerScore += 1;
-    console.log(`computer wins round`)};
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-    for (let i = 0; i <= 4; i++) {
 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+rock.addEventListener("click", (e) => {
+    humanSelection = e.target.textContent;
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+})
 
-    if (computerScore < humanScore) return 'Human wins bitch';
-    else if (computerScore > humanScore) return 'Computer wins beepbop';
-    else return 'its tie...damn';}
+paper.addEventListener("click", (e) => {
+    humanSelection = e.target.textContent;
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+})
 
-const btn = document.querySelector('#bob');
-btn.addEventListener("click", (e) => {
-    console.log(playGame());
+scissors.addEventListener("click", (e) => {
+    humanSelection = e.target.textContent;
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 })
